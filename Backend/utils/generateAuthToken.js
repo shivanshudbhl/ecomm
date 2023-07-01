a2b88029ccbfe270cab5d8f1721416cdf9b2d312
shivanshudbhl <88283486+shivanshudbhl@users.noreply.github.com>
2023-07-01 16:12:17 +0530

    changes

--- Backend/utils/generateAuthToken.js
+++ Backend/utils/generateAuthToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const generateAuthToken = (_id, name, lastName, email, isAdmin) => {
   return jwt.sign(
     { _id, name, lastName, email, isAdmin },
-    process.env.JWT_SECRET_KEY,
+    `${process.env.JWT_SECRET_KEY}`,
     { expiresIn: "7h" }
   );
 };
